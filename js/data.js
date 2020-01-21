7bed3aae3f68de7d40817e3128b0f61f277f8aa5
Maxime Cohen <maxime.cohen2@epsi.fr>
2020-01-21 10:37:59 +0100

    + Change Methods generation

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-var backgroundTextures = [
+let backgroundTextures = [
     "images/background/1.jpg",
     "images/background/2.jpg",
     "images/background/3.jpg",
@@ -7,61 +7,73 @@ var backgroundTextures = [
     "images/background/6.jpg"
 ];
 
-var data = {
+let data = {
     'systemSolar': {
-        
     },
     'sun': {
         color: "images/sun_color.jpg",
-        parent: 'systemSolar'
+        parent: 'systemSolar',
+        mesh: 'sphere'
     },
     'mercury': {
         color: "images/mercury_color.jpg",
-        parent: 'sun'
+        parent: 'sun',
+        mesh: 'sphere'
     },
     'venus': {
         color: "images/venus_color.jpg",
-        parent: 'sun'
+        parent: 'sun',
+        mesh: 'sphere'
     },
     'earth': {
         color: "images/earth_color.jpg",
-        parent: 'sun'
+        parent: 'sun',
+        mesh: 'sphere'
     },
     'earthCloud': {
         color: "images/earth_cloud_color.jpg",
-        parent: 'earth'
+        parent: 'earth',
+        mesh: 'sphere'
     },
     'moon': {
         color: "images/moon_color.jpg",
-        parent: 'earth'
+        parent: 'earth',
+        mesh: 'sphere'
     },
     'mars': {
         color: "images/mars_color.jpg",
-        parent: 'sun'
+        parent: 'sun',
+        mesh: 'sphere'
     },
     'jupiter': {
         color: "images/jupiter_color.jpg",
-        parent: 'sun'
+        parent: 'sun',
+        mesh: 'sphere'
     },
     'saturn': {
         color: "images/saturn_color.jpg",
-        parent: 'sun'
+        parent: 'sun',
+        mesh: 'sphere'
     },
     'saturnRings': {
         color: "images/saturn_rings_color.jpg",
-        parent: 'saturn'
+        parent: 'saturn',
+        mesh: 'ring'
     },
     'uranus': {
         color: "images/uranus_color.jpg",
-        parent: 'sun'
+        parent: 'sun',
+        mesh: 'sphere'
     },
     'uranusRings': {
         color: "images/uranus_rings_color.jpg",
-        parent: 'uranus'
+        parent: 'uranus',
+        mesh: 'ring'
     },
     'neptune': {
         color: "images/neptune_color.jpg",
-        parent: 'sun'
+        parent: 'sun',
+        mesh: 'sphere'
     }
 };
 
